6c0a5e04cafa06caeee78c1acb89ae7dccc3a47b
BrentGrammer <brentmarquez@gmail.com>
2019-08-27 10:25:55 -0600

    use @HostBinding instead of renderer in directive

--- src/app/better-directive/better-directive.directive.ts
+++ src/app/better-directive/better-directive.directive.ts
@@ -3,36 +3,42 @@ import {
   OnInit,
   Renderer2,
   ElementRef,
-  HostListener
+  HostListener,
+  HostBinding
 } from "@angular/core";
 
 @Directive({
   selector: "[appBetterHighlight]"
 })
 export class BetterHighlightDirective implements OnInit {
+  // remember to set an initial value to prevent errors:
+  @HostBinding("style.backgroundColor") backgroundColor: string = "blue";
+
   constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
 
   // pass in elref, which style to set, value, optional: flags
   ngOnInit() {
-    this.renderer.setStyle(
-      this.elementRef.nativeElement,
-      "background-color",
-      "blue"
-    );
+    // this.renderer.setStyle(
+    //   this.elementRef.nativeElement,
+    //   "background-color",
+    //   "blue"
+    // );
   }
 
   @HostListener("mouseenter") mouseover(eventData: Event) {
-    this.renderer.setStyle(
-      this.elementRef.nativeElement,
-      "background-color",
-      "purple"
-    );
+    // this.renderer.setStyle(
+    //   this.elementRef.nativeElement,
+    //   "background-color",
+    //   "purple"
+    // );
+    this.backgroundColor = "purple";
   }
   @HostListener("mouseleave") mouseleave(eventData: Event) {
-    this.renderer.setStyle(
-      this.elementRef.nativeElement,
-      "background-color",
-      "blue"
-    );
+    // this.renderer.setStyle(
+    //   this.elementRef.nativeElement,
+    //   "background-color",
+    //   "blue"
+    // );
+    this.backgroundColor = "blue";
   }
 }
